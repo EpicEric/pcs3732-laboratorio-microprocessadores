b5bf3a28a41983f2b798deb82b06b6b9539ff3ac
Eric Pires <ericpires9@gmail.com>
2017-07-11 12:30:38 -0300

    Exp8: Added pre-lab code

---
+++ 8/imprime.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void imprime(int i);
+
+int main() {
+	imprime(5);
+}
+
+void imprime(int i) {
+	if (i <= 0) {
+		return;
+	}
+	printf("Imprimi %d\n", i);
+	imprime(i - 1);
+}
