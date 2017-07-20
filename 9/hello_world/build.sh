596ed2a6d19610624226049ea088ed7d30956c81
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2017-07-20 14:07:10 -0300

    Add bare metal hello world

---
+++ 9/hello_world/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+arm-none-eabi-gcc -c -mcpu=arm926ej-s -g c_entry.c
+arm-none-eabi-as -c -mcpu=arm926ej-s -g startup.s -o startup.o
+arm-none-eabi-ld -T vector_table.ld c_entry.o startup.o -o test.elf
+arm-none-eabi-objcopy -O binary test.elf test.bin
