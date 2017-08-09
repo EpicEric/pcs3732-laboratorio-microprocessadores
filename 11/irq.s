cc0441a9c4c759936c4934bd074536d3138ceed9
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2017-08-09 11:20:58 -0300

    Remove unused supervisor check on save_process_state

--- 11/irq.s
+++ 11/irq.s
@@ -83,20 +83,17 @@ save_process_state:
 	@ Salva modo, muda para supervisor e salva banked registers
 	MOV     r2, r14                 @ r2 referencia tabela de registradores
 	MRS     r1, cpsr
-	AND	r3, r1, #0x1f
-	CMP	r3, #0x13		@ Ja em modo supervisor?
-	BEQ	skip_mode_change	@ Pula mudanca de modo
 	MSR     cpsr_ctl, #0b11010011   @ Supervisor, I = 1
-	skip_mode_change:
 	STMIA   r2!, {sp, lr}
 	MSR     cpsr, r1
 
-	CMP	r3, #0x13		@ Ja em modo supervisor?
+	AND	r1, r1, #0x1f
+	CMP	r1, #0x13		@ Ja em modo supervisor?
 	BEQ	save_cpsr		@ Salva cpsr em spsr da tabela de registradores
-	MRS     r3, spsr                @ Carrega spsr
+	MRS     r1, spsr                @ Carrega spsr
 	save_cpsr:
 	LDR     r0, irq_return_address
-	STMIA   r2!, {r0, r3}		@ Salva lr original e cpsr
+	STMIA   r2!, {r0, r1}		@ Salva lr original e cpsr
 	LDMFD	sp!, {pc}
 
 
