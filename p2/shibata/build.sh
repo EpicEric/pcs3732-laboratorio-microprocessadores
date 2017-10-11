1a3f9fc403ec197fda8471caad4118f31192c0f6
Eric Pires <eric.pires@devtecnologia.com.br>
2017-10-11 16:10:36 -0300

    Improved names

--- p2/shibata/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -ex
-arm-none-eabi-gcc -c -mcpu=arm926ej-s -g handler.c
-arm-none-eabi-as -c -mcpu=arm926ej-s -g irq.s -o irq.o
-arm-none-eabi-as -c -mcpu=arm926ej-s -g timer_init.s -o timer_init.o
-arm-none-eabi-ld -T linker.ld handler.o irq.o timer_init.o -o test.elf
-arm-none-eabi-objcopy -O binary test.elf test.bin
